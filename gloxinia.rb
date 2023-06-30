11158b72f6b7a095f5d9fb0de9e982c2df6fb192
Mathieu STUMPF-GUNTZ <psychoslave@culture-libre.org>
2023-07-01 01:51:41 +0200

    Identify most tokens bar strings and numbers

--- gloxinia.rb
+++ gloxinia.rb
@@ -4,16 +4,59 @@
 # https://craftinginterpreters.com/contents.html
 require 'readline'
 
+Brackets = {
+   openening·parenthese: %i[(],
+   closing·parenthese: %i[)],
+   openening·brace: %i[{],
+   closing·brace: %i[}],
+}
+Syncategoremata = %i[
+  and class else false fun for if nil or
+  print return super this true var while
+]
+Componing·operators = {
+  # binary infix ones
+  subordination: %i[.],
+  seriation: %i[,],
+  subtraction: %i[-],
+  addition: %i[+],
+  division: %i[/ ÷],
+  multiplication: %i[* ×],
+  # unary prefixed ones
+  opposition: %i[-],
+  negation: %i[~ ! ¬],
+  equapositivization: %i[+],
+  # unary suffixed one
+  termination: %i[;],
+}
+
+Relational·operators = {
+  assignment: %i[= ←],
+  difference: %i[!= ≠],
+  equality: %i[== ≘],
+  minimality: %i[<= ⩽],
+  maximality: %i[>= ⩾],
+  exceedingness: %i[>],
+  underness: %i[<],
+}
+
+Operators = [Relational·operators.values, Componing·operators.values].flatten
+
+Taxnomy = [
+  %i[identifier string number dyadee eschatophore],
+  Brackets.keys, Syncategoremata, Componing·operators.keys, Relational·operators.keys
+].reduce(&:+).compact
+
 module Scanner
-  def tokens
-    self.scan(/[^\s]+/).lazy
-  end
+  def tokens = self.scan(/\w+|[[:punct:]]/).lazy
 end
 
 Notification = Struct.new(:method, :context, :code ) do
-  def ply
-    send(method, "error: #{context}: #{code}")
-  end
+  def ply = send(method, "error: #{context}: #{code}")
+end
+
+Lexie = Struct.new *%i[type lexeme literal locus] do
+  def to_s = [type, lexeme, literal].join ' '
 end
 
 class Gloxinia
@@ -24,10 +67,25 @@ class Gloxinia
   end
 
   def run_file(filename)
-    File.readlines(filename).each.with_index do |line, row|
-      run line
-      Notification.new(:abort, "#{filename}:#{row+1}", line).ply unless @steady
+    File.readlines(filename).each.with_index(1) do |line, row|
+      run line, row
+      Notification.new(:abort, "#{filename}:#{row}", line).ply unless @steady
     end
+  end
+
+  def categorize(token)
+    #TODO: check for string literal
+    [Brackets, Componing·operators, Relational·operators].each do |hash|
+      hit = hash.keys.find { |key| hash[key].include?(token) }
+      return hit unless not hit
+      #puts ">info: '#{token}' not in #{hash.values.flatten}"
+    end
+
+    return :syncategoreme if Syncategoremata.include?(token)
+    #puts ">info: '#{token}' not in reserved words"
+
+    return :identifier
+
   end
 
   def run_repl
@@ -41,8 +99,12 @@ class Gloxinia
     end
   end
 
-  def run(code)
-    puts code.extend(Scanner).tokens.to_a
+  def run(code, ordinate)
+    lexies = code.extend(Scanner).tokens.map { |token|
+      type = categorize(token.to_sym)
+      Lexie.new(type, token, token, ordinate)
+    }
+    lexies.each{puts _1}
   end
 
   def print_usage
